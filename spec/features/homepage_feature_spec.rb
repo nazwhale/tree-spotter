c9a3880ffefc4c9189ed137f5650dfbb66ab019d
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-05-11 12:10:25 +0100

    Added homepage

---
+++ spec/features/homepage_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'homepage' do
+  context 'displays a welcome message' do
+    scenario "should say 'Welcome to Tree Spotter!'" do
+      visit '/photos'
+      expect(page).to have_content 'Welcome to Tree Spotter!'
+      expect(page).to have_link 'Upload photo'
+    end
+  end
+end
